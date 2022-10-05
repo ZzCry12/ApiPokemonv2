56f9bda8a5d98bdfc1e957c19957ac42ac36658b
ZzCry12 <dariorata@gmail.com>
2022-10-05 12:31:52 +0200

    v1.0

---
+++ app/src/main/java/com/example/apipokemon/FirstFragment.java
@@ -0,0 +1,105 @@
+package com.example.apipokemon;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.view.MenuHost;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.apipokemon.databinding.FragmentFirstBinding;
+
+import java.util.ArrayList;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class FirstFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+    private ArrayAdapter<Pokemon> adapter;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+
+        return binding.getRoot();
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+
+
+        ArrayList<Pokemon> items = new ArrayList<>();
+
+        adapter = new ArrayAdapter<Pokemon>(
+                getContext(),
+                R.layout.lv_pokemon_row,
+                R.id.txtListName,
+                items
+        );
+
+        binding.listview1.setAdapter(adapter);
+
+        refresh();
+        setHasOptionsMenu(true);
+
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+
+
+
+    public  boolean onOptionItemSelected(@NonNull MenuItem item){
+        if(item.getItemId()== R.id.refresh){
+            refresh();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void refresh() {
+
+        Toast.makeText(getContext(),"Refreshing..", Toast.LENGTH_LONG).show();
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+
+
+        executor.execute(() -> {
+            PokemonApi api = new PokemonApi();
+            ArrayList<Pokemon> pokemons = api.getPokemons();
+
+            System.out.println(pokemons);
+
+
+
+
+            handler.post(() -> {
+                adapter.clear();
+                adapter.addAll(pokemons);
+            });
+
+        });
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
